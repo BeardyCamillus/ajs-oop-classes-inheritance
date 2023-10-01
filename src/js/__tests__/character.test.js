2c5c5529ba448ba034f334eb742dac9ddd1d4196
BeardyCamillus <beardy.camillus@gmail.com>
2023-10-01 22:13:17 +0300

    test fixes

--- src/js/__tests__/character.test.js
+++ src/js/__tests__/character.test.js
@@ -1,22 +1,23 @@
+import Bowman from '../bowman';
 import Character from '../character';
 
 test('Создание персонажа с именем меньше 2 символов', () => {
   expect(() => {
-    const character = new Character('A', 'Bowman', 25, 25);
+    const character = new Bowman('A');
     return character;
   }).toThrow(new Error('Имя должно содержать минимум 2 символа!'));
 });
 
 test('Создание персонажа с именем больше 10 символов', () => {
     expect(() => {
-      const character = new Character('Abcdefghijklm', 'Bowman', 25, 25);
+      const character = new Bowman('Abcdefghijklm');
       return character;
     }).toThrow(new Error('Имя может содержать максимум 10 символов!'));
   });
 
 test('Создание персонажа с неверным типом', () => {
   expect(() => {
-    const character = new Character('bowman', 'Orc', 25, 25);
+    const character = new Character('Test', 'Orc');
     return character;
   }).toThrow(new Error('Неверный тип персонажа!'));
 });
